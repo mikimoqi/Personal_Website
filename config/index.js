7619c3d5c43d8a03ae23c1aabc0f33523c8861f7
Jie Liang Yin <miki-1344@hotmail.com>
2021-06-08 00:58:26 -0700

    prod rdy

---
+++ config/index.js
@@ -0,0 +1,88 @@
+module.exports = {
+  //-- SITE SETTINGS -----
+  author: "Miki",
+  siteTitle: "Miki.Portfolio",
+  siteShortTitle: "Home", // Used as logo text in header, footer, and splash screen
+  siteDescription: "Miki's Developer Portfolio",
+  siteUrl: "https://miki.netlify.app/",
+  siteLanguage: "en_US",
+  siteIcon: "content/favicon1.png", // Relative to gatsby-config file
+  seoTitleSuffix: "Miki's Portfolio", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
+  useCookieBar: false, // If you use Google Analytics and want to be GDPR-compliant, set it to true
+  googleAnalyticsTrackingId: "", // e.g. UA-XXXXXX-X
+
+  // -- THEME SETTINGS -----
+  colors: {
+    lightTheme: {
+      primary: "#000000",
+      secondary: "#FFF4D9",
+      tertiary: "#F2F2F2",
+      text: "#000000",
+      subtext: "#555555",
+      background: "#FFFFFF",
+      card: "#FFFFFF",
+      scrollBar: "rgba(0, 0, 0, 0.5)",
+      boxShadow: "rgba(0, 0, 0, 0.16)",
+      boxShadowHover: "rgba(0, 0, 0, 0.32)",
+    },
+    darkTheme: {
+      primary: "#FAFAFA",
+      secondary: "#2A2926",
+      tertiary: "#252525",
+      text: "rgba(255, 255, 255, 0.87)",
+      subtext: "#AAAAAA",
+      background: "#121212",
+      card: "#1C1C1C",
+      scrollBar: "rgba(255, 255, 255, 0.5)",
+      boxShadow: "rgba(0, 0, 0, 0.16)",
+      boxShadowHover: "rgba(0, 0, 0, 0.32)",
+    },
+  },
+  fonts: {
+    primary: "Roboto, Arial, sans-serif",
+  },
+
+  //-- ARTICLES SECTION SETTINGS -----
+  // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
+  // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
+  mediumRssFeed:
+    "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantinmuenster",
+  // rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.theguardian.com%2Finternational%2Frss",
+
+  shownArticles: 3,
+
+  //-- SOCIAL MEDIA SETTINGS -----
+  // There are icons available for the following platforms:
+  // Medium, GitHub, LinkedIn, XING, Behance, E-Mail
+  socialMedia: [
+    {
+      name: "Linkedin",
+      url: "https://www.linkedin.com/in/miki-yin/",
+    },
+    {
+      name: "Github",
+      url: "https://github.com/supermikii",
+    },
+  ],
+
+  //-- NAVIGATION SETTINGS -----
+  navLinks: {
+    menu: [
+      {
+        name: "About Me",
+        url: "/#about",
+      },
+      {
+        name: "Projects",
+        url: "/#projects",
+      },
+    ],
+    button: {
+      useFileName: true,
+      name: "Resume",
+      fileName: "MikiResume.png", // the file has to be placed inside the static folder at the root level
+      url: "", // if useFileName=false, you can set an anchor link here and use the button for navigational purposes
+    },
+  },
+  footerLinks: [],
+}
